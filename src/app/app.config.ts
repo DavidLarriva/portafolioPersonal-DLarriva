29d6da6c42e440c3948cd8566e07c1384844b78b
DavidLarriva <dalarrivac@gmail.com>
2026-06-13 15:32:02 -0500

    configuracion httpclient para consumo de apis

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    // encender la herramienta para pedir datos a internet
+    provideHttpClient(withFetch())
   ]
 };
