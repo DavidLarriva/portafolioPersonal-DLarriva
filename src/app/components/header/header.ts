5122840201190ad93a3eeeab97539d99fd38d2f3
DavidLarriva <dalarrivac@gmail.com>
2026-06-16 10:19:50 -0500

    actualizar header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrls: ['./header.css']
 })
-export class HeaderComponent {
-  // logica del encabezado
+export class AppHeaderComponent {
+  
+  // pedimos prestadas las herramientas de sesion y navegacion
+  authService = inject(AuthService);
+  private router = inject(Router);
+
+  // funcion para salir de la cuenta y volver al inicio
+  cerrarSesion() {
+    this.authService.logout().subscribe(() => {
+      this.router.navigate(['/']);
+    });
+  }
 }
